a12ff8853d1861fcc4c277e08f2bb73e69b92ffe
Christian Blais <christ.blais@gmail.com>
2018-02-01 13:18:53 -0500

    New Pseudolocalization I18n backend

--- lib/pseudolocalization.rb
+++ lib/pseudolocalization.rb
@@ -1,5 +1,67 @@
 require "pseudolocalization/version"
 
 module Pseudolocalization
-  # Your code goes here...
+  module I18n
+    class Backend
+      LEET = {
+        'a' => 'α',
+        'b' => 'β',
+        'c' => 'ͼ',
+        'd' => 'd',
+        'e' => 'ε',
+        'f' => 'ϝ',
+        'g' => 'g',
+        'h' => 'h',
+        'i' => '1',
+        'j' => 'ϳ',
+        'k' => 'Ϗ',
+        'l' => 'l',
+        'm' => 'ϻ',
+        'n' => 'ͷ',
+        'o' => '0',
+        'p' => 'ρ',
+        'q' => 'q',
+        'r' => 'r',
+        's' => 's',
+        't' => 'ϯ',
+        'u' => 'u',
+        'v' => 'v',
+        'w' => 'ω',
+        'x' => 'x',
+        'y' => 'ϒ',
+        'z' => 'z',
+      }
+
+      def initialize(old_backend)
+        @old_backend = old_backend
+      end
+
+      def method_missing(name, *args, &block)
+        if respond_to_missing?(name)
+          @old_backend.public_send(name, *args, &block)
+        else
+          super
+        end
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        @old_backend.respond_to?(name) || super
+      end
+
+      def translate(locale, key, options)
+        result = @old_backend.translate(locale, key, options)
+
+        return result unless result.is_a?(String)
+
+        # replace html entities, we don't care
+        result = result.gsub(/&[a-z]+;/, ' ')
+
+        # double all vowels in an attempt to increase words length
+        result = result.gsub(/[aeiouy]/) { |c| c * 2 }
+
+        # replace chars with utf8 lookalike
+        result.gsub(/[a-z]/, LEET)
+      end
+    end
+  end
 end
--- test/pseudolocalization_test.rb
+++ test/pseudolocalization_test.rb
@@ -1,11 +1,21 @@
 require "test_helper"
 
 class PseudolocalizationTest < Minitest::Test
+  class DummyBackend
+    def translate(_locale, string, _options)
+      string
+    end
+  end
+
+  def setup
+    @backend = Pseudolocalization::I18n::Backend.new(DummyBackend.new)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Pseudolocalization::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_pseudolocalizes
+    assert_equal 'Hεεll00, ω00rld!', @backend.translate(:en, 'Hello, world!', {})
   end
 end
